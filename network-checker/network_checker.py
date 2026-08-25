a08891135f3ba6512ad91b291ac35c90f05e9dc5
Victor Rodriguez <victorro1220@gmail.com>
2026-08-25 13:05:09 -0600

    Add DNS resolution to Network Checker

--- network-checker/network_checker.py
+++ network-checker/network_checker.py
@@ -1,6 +1,10 @@
+import socket
+
 print("Network Checker")
 
 host = input("Enter a host or website: ")
 
-print("Checking:", host)
-print("Status: Test started")
+ip_address = socket.gethostbyname(host)
+
+print("Host:", host)
+print("IP Address:", ip_address)
