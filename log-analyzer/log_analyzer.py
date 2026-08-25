13124620165e88fc6e35955b1c7a980c293dc222
Victor Rodriguez <victorro1220@gmail.com>
2026-08-25 14:29:53 -0600

    Add initial Log Analyzer

---
+++ log-analyzer/log_analyzer.py
@@ -0,0 +1,24 @@
+log_file = "log-analyzer/sample.log"
+
+info_count = 0
+warning_count = 0
+error_count = 0
+
+with open(log_file, "r") as file:
+    for line in file:
+
+        if "ERROR" in line:
+            error_count += 1
+
+        elif "WARNING" in line:
+            warning_count += 1
+
+        elif "INFO" in line:
+            info_count += 1
+
+print("Log Analyzer")
+print("\n--- Summary ---")
+
+print("INFO:", info_count)
+print("WARNING:", warning_count)
+print("ERROR:", error_count)
