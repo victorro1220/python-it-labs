923a87990bb893604c2d7c5c2ed094e47639f07c
Victor Rodriguez <victorro1220@gmail.com>
2026-08-25 14:18:40 -0600

    Add CPU memory and disk monitoring

--- system-health-checker/system_health_checker.py
+++ system-health-checker/system_health_checker.py
@@ -1,5 +1,6 @@
 import platform
 import socket
+import psutil
 
 print("System Health Checker")
 
@@ -9,8 +10,20 @@ os_version = platform.version()
 architecture = platform.machine()
 local_ip = socket.gethostbyname(computer_name)
 
+cpu_usage = psutil.cpu_percent(interval=1)
+
+memory = psutil.virtual_memory()
+ram_usage = memory.percent
+
+disk = psutil.disk_usage("C:\\")
+disk_usage = disk.percent
+
 print("Computer Name:", computer_name)
 print("Operating System:", operating_system)
 print("OS Version:", os_version)
 print("Architecture:", architecture)
 print("Local IP:", local_ip)
+
+print("CPU Usage:", cpu_usage, "%")
+print("RAM Usage:", ram_usage, "%")
+print("Disk Usage:", disk_usage, "%")
